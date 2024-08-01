25f7a65dc36c22853822ec31c9106b547c2d5818
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 06:08:28 +0000

    Code generation

--- components/simple_cta_component_1722492348.js
+++ components/simple_cta_component_1722492348.js
@@ -16,6 +16,7 @@ Vue.component("simple_cta_component_1722492348", {
                 <div id="cta-button-container" class="flex space-x-4">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Browse Recipes</a>
                     <a href="#" class="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Nine</a>
+                    <a href="#" class="flex-1 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Zero</a>
                 </div>
             </div>
         </div>
