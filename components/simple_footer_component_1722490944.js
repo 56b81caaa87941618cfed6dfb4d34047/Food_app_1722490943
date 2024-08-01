5b3787efd00085e220a6a769b2f11dce05ccfbc0
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 05:42:32 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722490944.js
+++ components/simple_footer_component_1722490944.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722490944", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            FoodFinder - Your Culinary Companion
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Discover new flavors, explore diverse cuisines, and embark on a delicious journey with FoodFinder. Your go-to app for all things food-related.
                     </div>
                 </div>
             </div>
