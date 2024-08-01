4411d013133c6a5283049ea6df400c98559cd882
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 06:06:25 +0000

    Code generation

--- components/simple_cta_component_1722492348.js
+++ components/simple_cta_component_1722492348.js
@@ -13,8 +13,9 @@ Vue.component("simple_cta_component_1722492348", {
                     <p id="cta-subtitle" class="flex-1 mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Explore our collection of mouthwatering recipes and find your next culinary adventure.</p>
                 </div>
                 
-                <div id="cta-button-container" class="flex">
+                <div id="cta-button-container" class="flex space-x-4">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Browse Recipes</a>
+                    <a href="#" class="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Nine</a>
                 </div>
             </div>
         </div>
