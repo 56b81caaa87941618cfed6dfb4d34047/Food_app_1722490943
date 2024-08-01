bc1966a7b69c366370a44dc4d07e1589c86c86c0
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 06:12:08 +0000

    Merge code from frontend

--- components/simple_cta_component_1722492348.js
+++ components/simple_cta_component_1722492348.js
@@ -6,7 +6,7 @@ Vue.component("simple_cta_component_1722492348", {
         <div id="cta-inner-container" class="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
             <div id="cta-content" class="max-w-screen-sm mx-auto text-center">
                 <div id="cta-title-container" class="flex">
-                    <h2 id="cta-title" class="flex-1 mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">Discover Delicious Flavors</h2>
+                    <h2 id="cta-title" class="flex-1 mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">Discover Delicious Flavors12</h2>
                 </div>
 
                 <div id="cta-subtitle-container" class="flex">
